00a7810a74366a186610c6f753924445f98617dc
chenyi1976 <yichen1976@gmail.com>
2012-11-21 09:49:59 +1100

    - update douban link plugin for new jyson library

--- resources/plugin/ItemDetailButton_doubanlink.py
+++ resources/plugin/ItemDetailButton_doubanlink.py
@@ -2,13 +2,16 @@
 # get the douban.com id by imdb.com id
 # NOT working, due to jython does not support ssl currently.
 ######################################################################
-import json
+
+#jython 2.5 does not support json, we use third party json implementation from http://jyson.xhaus.com/
+import com.xhaus.jyson.JysonCodec as json
+#import json
 import urllib2
 
 ATTRIBUTE_IMDB='imdb_id'
 JSON_ATTR_ALT='alt'
 JSON_ATTR_ID='id'
-JSON_ID_PREFIX='http:\/\/api.douban.com\/movie\/'
+JSON_ID_PREFIX='http://api.douban.com/movie/'
 
 currentMovieId = getCurrentMovieId()
 print 'currentMovieId:' + str(currentMovieId)
@@ -23,6 +26,6 @@ douban_url=data.get(JSON_ATTR_ALT)
 douban_id=data.get(JSON_ATTR_ID)
 print douban_id
 if douban_id.startswith(JSON_ID_PREFIX):
-    douban_id=douban_id[len(JSON_ID_PREFIX):]
+    douban_id=douban_id(len(JSON_ID_PREFIX))
 print douban_id
-#os.system(douban_url)
+#we need to add the douban_id to db here.
